79c2edb22d8c5c22040052690722b71888580dcf
L1n <490772448@qq.com>
2016-10-02 10:44:32 +0800

    首页和文章页可以链接起来了

--- my_blog/articles/views.py
+++ my_blog/articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 from .models import Article
+import datetime
 
 
 def home(request):
@@ -8,9 +10,9 @@ def home(request):
     return render(request, 'home.html', {'post_list': post_list})
 
 
-def detail(request, my_args):
-    db_data = Article.objects.all()[int(my_args)]
-    message = "title = {}, category = {}, date_time = {}, content = {}".format(db_data.title, db_data.category,
-                                                                               db_data.date_time,
-                                                                               db_data.content)
-    return HttpResponse(message)
+def detail(request, article_number):
+    try:
+        db_data = Article.objects.get(id=article_number)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, "article.html", {"post": db_data})
