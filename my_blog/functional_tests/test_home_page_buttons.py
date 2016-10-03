8cca8dab6f588a34af7d35bddbce927c50656100
L1n <490772448@qq.com>
2016-10-03 11:00:49 +0800

    就差归档按钮的功能测试了, 其他的都已经编写好并且通过测试了

--- my_blog/functional_tests/test_home_page_buttons.py
+++ my_blog/functional_tests/test_home_page_buttons.py
@@ -12,9 +12,12 @@ __author__ = '__L1n__w@tch'
 
 
 class TestHomePageButtons(FunctionalTest):
-    def test_home_page_button(self):
+    def setUp(self):
+        super().setUp()
         # Y 访问首页
         self.browser.get(self.server_url)
+
+    def test_home_page_button(self):
         home_page_url = self.browser.current_url
         home_page_source = self.browser.page_source
 
@@ -32,8 +35,6 @@ class TestHomePageButtons(FunctionalTest):
         self.assertNotEqual(self.browser.page_source, home_page_source)
 
     def test_about_me_button(self):
-        # Y 访问首页
-        self.browser.get(self.server_url)
         home_page_source = self.browser.page_source
 
         # 看到 about_me 按钮, 点击, 发现界面有所变化, 并且显示了作者的相关信息, 特别注意到了座右铭: "Valar Morghulis"
@@ -43,34 +44,31 @@ class TestHomePageButtons(FunctionalTest):
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn(r"座右铭: Valar Morghulis", [row.text for row in rows])
 
-    @unittest.skipIf(True, "还没开始编写")
     def test_github_button(self):
-        # Y 访问首页
-
         # 看到 github 按钮, 点击, 发现确实跳转到了一个 github 页面上, 而且 github 页面的用户是 "L1nwatch"
-
-        pass
+        self.browser.find_element_by_id("id_github").click()
+        self.assertIn("github", self.browser.current_url)
+        self.assertIn("L1nwatch", self.browser.current_url)
 
     @unittest.skipIf(True, "还没开始编写")
     def test_archive_button(self):
-        # Y 访问首页
+        home_page_source = self.browser.page_source
 
         # 看到了归档按钮, 不知道有什么用, 点击看看
+        self.browser.find_element_by_id("id_archive").click()
 
         # 看见现在每一个文章都不会显示内容了, 而只是显示标题/时间等信息
+        self.assertNotEqual(self.browser.page_source, home_page_source)
 
         pass
 
-    @unittest.skipIf(True, "还没开始编写")
     def test_email_button(self):
-        # Y 访问首页
-
         # Y 想联系网站拥有者, 发现了个 email 按钮
+        email_button = self.browser.find_element_by_id("id_email")
 
         # 点击后发现页面跳转了, 而且可以看到 url 中有个 mail to 字母, 后面跟着作者的邮箱: "watch1602@gmail.com"
-
-        pass
-
+        # 发现如果使用 click 会使用默认浏览器打开然后就无法测试了, 所以就改用下面这种方法
+        self.assertEqual("mailto:watch1602@gmail.com", email_button.get_attribute("href"))
 
 if __name__ == "__main__":
     pass
