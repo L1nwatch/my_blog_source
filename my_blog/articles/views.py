67d24117690f1ca06a86bd0de09f2e006eed07ff
L1n <490772448@qq.com>
2016-10-05 23:03:20 +0800

    为了调试!

--- my_blog/articles/views.py
+++ my_blog/articles/views.py
@@ -122,7 +122,7 @@ def __is_valid_md_file(file_name):
 
 
 def update_notes(request):
-    locale.setlocale(locale.LC_ALL, "zh_CN.utf8")
+    os.system("export LANG=zh_CN.utf8")
 
     __get_latest_notes()
 
