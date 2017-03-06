3c26c6f5a39908e7a066b99ded56de4bcc00650d
L1n <490772448@qq.com>
2017-03-06 08:53:28 +0800

    更新 admin 管理相关代码

--- deploy_tools/test_user_pass_reader.py
+++ deploy_tools/test_user_pass_reader.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 # version: Python3.X
 """
+2017.03.05 新增了一个 fab 配置 gitbooks 的操作, 但还是懒得写测试, 于是依旧手调
 2017.02.10 写测试好懒, 还是手调吧
 2017.02.03 由于自己的 git 为私有 git, 需要通过配置文件读取 git 账号密码, 所以需要编写函数进行测试
 """
-from fabfile import _user_pass_file_config,_gitbooks_config
+from fabfile import _user_pass_file_config, _gitbooks_config
 
 __author__ = '__L1n__w@tch'
 
 if __name__ == "__main__":
     # _user_pass_file_config()
-    _gitbooks_config("/Users/L1n/Desktop/Code/Python/my_blog_source",site_name="my_blog")
+    _gitbooks_config("/Users/L1n/Desktop/Code/Python/my_blog_source", site_name="my_blog")
--- my_blog/gitbook_notes/admin.py
+++ my_blog/gitbook_notes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from gitbook_notes.models import GitBook
+
+
+class GitBookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'book_name', "href", "md_file_name")
+    search_fields = ('title', 'category', 'book_name', 'md_file_name', "content")
+    list_filter = ("book_name",)
+
+
+admin.site.register(GitBook, GitBookAdmin)
