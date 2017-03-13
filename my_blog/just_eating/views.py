e7884b545f78ef3b9bc1bdd7a34cf9b37777e857
L1n <490772448@qq.com>
2017-03-13 11:05:44 +0800

    更新菜单

--- my_blog/just_eating/views.py
+++ my_blog/just_eating/views.py
@@ -37,13 +37,13 @@ def create_home_menu():
 def create_school_menu():
     school_menu = list()
 
-    monday = const.EATING_MENU_STRUCTURE("周一", "丁香\n杂粮煎饼\n豆浆", "新综\n香干炒肉(conflict)", "竹园\n蒙古烤肉")
+    monday = const.EATING_MENU_STRUCTURE("周一", "丁香\n杂粮煎饼\n豆浆", "新综\n香干炒肉", "竹园\n蒙古烤肉")
     tuesday = const.EATING_MENU_STRUCTURE("周二", "竹园\n手抓饼\n牛奶", "海棠\n川渝-腐竹炒肉", "(美食坊|丁香|海棠)\n快餐")
     wednesday = const.EATING_MENU_STRUCTURE("周三", "海棠\n卤肉卷\n豆浆", "海棠\n川味椒盐里脊", "新综\n小米鸡排饭")
     thursday = const.EATING_MENU_STRUCTURE("周四", "海棠\n杂粮煎饼\n豆浆", "丁香\n农家小炒肉", "新综\n快餐")
     friday = const.EATING_MENU_STRUCTURE("周五", "海棠\n照烧鸡腿饼\n豆浆", "海棠\n二楼快餐|鱼香肉丝", "丁香\n三楼自选快餐")
     saturday = const.EATING_MENU_STRUCTURE("周六", "老综\n卤肉卷\n豆浆", " 海棠\n川渝-辣子鸡", " 新综\n包菜肉片")
-    sunday = const.EATING_MENU_STRUCTURE("周日", "新综\n安仔包\n鸡肉+鲜汁肉\n豆浆", "海棠\n微辣香锅", "海棠\n川味盐煎肉")
+    sunday = const.EATING_MENU_STRUCTURE("周日", "新综\n安仔包\n鸡肉+鲜汁肉\n红枣豆浆", "海棠\n微辣香锅", "海棠\n川味香干肉丝")
 
     for each_day in [monday, tuesday, wednesday, thursday, friday, saturday, sunday]:
         school_menu.append(each_day)
