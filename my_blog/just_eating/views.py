3cbddfd21046636cecf56746b23d1c0b63a5892f
L1n <490772448@qq.com>
2017-04-03 09:23:23 +0800

    更新菜单

--- my_blog/just_eating/views.py
+++ my_blog/just_eating/views.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # version: Python3.X
 """
-2017.03.22 添加有关备选菜单的相关代码实线
+2017.03.22 添加有关备选菜单的相关代码实现
 2017.03.10 将记录日记的装饰器装饰到对应视图上
 """
 from django.http import Http404
@@ -46,13 +46,13 @@ def create_home_menu():
 def create_school_menu():
     school_menu = list()
 
-    monday = const.EATING_MENU_STRUCTURE("周一", "丁香\n杂粮煎饼\n豆浆", "新综\n香干炒肉(油!)", "竹园\n蒙古烤肉(油!)")
+    monday = const.EATING_MENU_STRUCTURE("周一", "丁香\n待定\n豆浆", "新综\n香干炒肉(油)", "竹园\n蒙古烤肉(油)")
     tuesday = const.EATING_MENU_STRUCTURE("周二", "海棠\n照烧鸡腿饼", "海棠\n川渝-杏鲍菇炒肉", "竹园\n快餐")
     wednesday = const.EATING_MENU_STRUCTURE("周三", "竹园\n手抓饼\n牛奶", "海棠\n川味椒盐里脊", "新综\n黑椒牛柳(油!)")
     thursday = const.EATING_MENU_STRUCTURE("周四", "海棠\n杂粮煎饼", "丁香\n农家小炒肉", "新综\n快餐")
-    friday = const.EATING_MENU_STRUCTURE("周五", "海棠\n花干菜夹馍\n豆浆", "待定", "丁香\n三楼自选快餐")
-    saturday = const.EATING_MENU_STRUCTURE("周六", "老综\n卤肉卷or土豆片夹馍\n", "老综\n029餐厅", " 新综\n包菜肉片(油!)")
-    sunday = const.EATING_MENU_STRUCTURE("周日", "新综+海棠\n安仔包\n俩鸡肉+豆浆", "新综\n鸡米花饭(带)", "待定")
+    friday = const.EATING_MENU_STRUCTURE("周五", "海棠\n花干菜夹馍\n豆浆", "新综\n台湾(油)", "丁香\n三楼自选快餐")
+    saturday = const.EATING_MENU_STRUCTURE("周六", "老综(海棠)\n卤肉卷\n", "老综\n029餐厅", " 新综\n包菜肉片(油!)")
+    sunday = const.EATING_MENU_STRUCTURE("周日", "新综\n安仔包\n俩鸡肉", "新综\n鸡米花饭(带)", "待定")
 
     for each_day in [monday, tuesday, wednesday, thursday, friday, saturday, sunday]:
         school_menu.append(each_day)
