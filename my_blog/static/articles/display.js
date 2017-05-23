5452b824e53d8642c72d5970927b75aa078795f3
L1n <490772448@qq.com>
2017-05-23 18:59:34 +0800

    修改文章显示界面左边目录树的样式, 现在可以支持子目录并且当目录比较小时会自动展开

--- my_blog/static/articles/display.js
+++ my_blog/static/articles/display.js
@@ -2,22 +2,51 @@
  * Created by L1n on 17/5/22.
  */
 
+function add_current_for_show(show_button) {
+    // 为 li 添加 current 方便显示下一级标题
+    $($(show_button).parent().parent()).toggleClass("current");
+    // 为 a 添加 current 方便样式改变
+    $($(show_button).parent()).toggleClass("current");
+
+    // 将当前的 + 号改为减号
+    $($(show_button)).toggleClass("fa-plus-square-o");
+    $($(show_button)).toggleClass("fa-minus-square-o");
+}
+
+function add_current_for_a_show(class_name, a_show) {
+    // 为 a 添加 current 方便显示跳转效果
+    $(class_name).removeClass("current");
+    $($(a_show)).toggleClass("current");
+}
+
 $(document).ready(function () {
-    $(".show-button").click(function () {
-        // 为 li 添加 current 方便显示下一级标题
-        $($(this).parent().parent()).toggleClass("current");
-        // 为 a 添加 current 方便样式改变
-        $($(this).parent()).toggleClass("current");
-
-        // 将当前的 + 号改为减号
-        $($(this)).toggleClass("fa-plus-square-o");
-        $($(this)).toggleClass("fa-minus-square-o");
+    // 为第 1 级目录树添加展开按钮相关代码
+    $(".show-button1").click(function () {
+        add_current_for_show($(this));
     });
 
-    $(".a-show").click(function () {
-        // 为 a 添加 current 方便显示跳转效果
-        $(".a-show").removeClass("current");
-        $($(this)).toggleClass("current");
+    // 为第 1 级目录树添加展开按钮相关代码
+    $(".a-show1").click(function () {
+        add_current_for_a_show(".a-show1", $(this));
     });
+
+    // 为第 2 级目录树添加展开按钮相关代码
+    $(".show-button2").click(function () {
+        add_current_for_show($(this));
+    });
+
+    // 为第 2 级目录树添加展开按钮相关代码
+    $(".a-show2").click(function () {
+        add_current_for_a_show(".a-show2", $(this));
+    });
+
+    // 判断一级目录是否有多个, 如果小于 3 个则自动展开
+    if ($(".toctree-l1").length < 3) {
+        add_current_for_show($(".show-button1"));
+        // 判断二级目录是否有多个, 如果小于 3 个则自动展开
+        if ($(".toctree-l2").length < 3) {
+            add_current_for_show($(".show-button2"));
+        }
+    }
 
 });
