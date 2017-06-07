053b67a9f01607b07f7191b928c64aed67df4853
L1n <490772448@qq.com>
2017-06-07 10:23:28 +0800

    修改 ToolHub 首页时钟样式, 给搜索结果的标签添加 Tag 搜索功能

--- my_blog/static/toolhub/js/home.js
+++ my_blog/static/toolhub/js/home.js
@@ -4,174 +4,77 @@ $(document).ready(function () {
     // $("#id_encoding_cipher").addClass("active");
 });
 
-Element.Properties.transform = {
+// 时钟的 js
+var Clock = (function () {
 
-    set: function (transform) {
-        var property = 'transform';
-        console.log(Browser);
-        if (Browser.chrome) property = 'WebkitTransform';
-        if (Browser.firefox)  property = 'MozTransform';
-        if (Browser.opera) property = 'OTransform';
+    var exports = function (element) {
+        this._element = element;
+        var html = '';
+        for (var i = 0; i < 6; i++) {
+            html += '<span>&nbsp;</span>';
+        }
+        this._element.innerHTML = html;
+        this._slots = this._element.getElementsByTagName('span');
+        this._tick();
+    };
 
-        this.style[property] = transform;
-        this.store('transform', transform);
-    },
+    exports.prototype = {
 
-    get: function () {
-        return this.retrieve('transform', '');
-    }
+        _tick: function () {
+            var time = new Date();
+            this._update(this._pad(time.getHours()) + this._pad(time.getMinutes()) + this._pad(time.getSeconds()));
+            var self = this;
+            setTimeout(function () {
+                self._tick();
+            }, 1000);
+        },
 
-};
+        _pad: function (value) {
+            return ('0' + value).slice(-2);
+        },
 
-Element.implement({
+        _update: function (timeString) {
 
-    setTransform: function (value) {
-        return this.set('transform', value);
-    },
+            var i = 0, l = this._slots.length, value, slot, now;
+            for (; i < l; i++) {
 
-    getTransform: function () {
-        return this.get('transform');
-    }
+                value = timeString.charAt(i);
+                slot = this._slots[i];
+                now = slot.dataset.now;
 
-});
+                if (!now) {
+                    slot.dataset.now = value;
+                    slot.dataset.old = value;
+                    continue;
+                }
 
-// $(window).addEvent('domready', function () {
-//     var $hourWrap = $$('.hour-wrap');
-//     var $hourFront = $hourWrap.getElement('div.front');
-//     var $hourBack = $hourWrap.getElement('div.back')
-//     var $hourTop = $hourWrap.getElement('div.digit-top');
-//     var $hourBottom = $hourWrap.getElement('div.digit-bottom .front');
-//
-//     var $minWrap = $$('.min-wrap');
-//     var $minFront = $minWrap.getElement('div.front');
-//     var $minBack = $minWrap.getElement('div.back');
-//     var $minTop = $minWrap.getElement('div.digit-top');
-//     var $minBottom = $minWrap.getElement('div.digit-bottom .front');
-//
-//
-//     var $secWrap = $$('.sec-wrap');
-//     var $secFront = $secWrap.getElement('div.front');
-//     var $secBack = $secWrap.getElement('div.back');
-//     var $secTop = $secWrap.getElement('div.digit-top');
-//     var $secBottom = $secWrap.getElement('div.digit-bottom .front');
-//
-//     var currentHour = 0;
-//     var currentMin = 0;
-//     var currentSec = 0;
-//
-//     var setClock = function () {
-//         var time = new Date();
-//         var hour = time.getHours();
-//         var min = time.getMinutes();
-//         var sec = time.getSeconds();
-//
-//         if (currentHour != hour) {
-//             currentHour = hour;
-//             var hourText = hour < 10 ? '0' + hour : hour;
-//
-//             // make el to sit behind the top digit
-//             var $newHourTop = new Element('div', {class: 'digit-top', html: $hourTop.get('html'), style: 'z-index:1;'})
-//             var $newHourFront = $newHourTop.getElement('div.front');
-//             var $newHourBack = $newHourTop.getElement('div.back');
-//
-//             $newHourFront.set('text', hourText);
-//             $hourWrap.adopt($newHourTop);
-//
-//             // start the animation
-//             $hourFront.setTransform('rotateX(180deg)');
-//             $hourBack.setTransform('rotateX(0deg)');
-//             $hourBack.setStyle('zIndex', 40);
-//
-//             // set the min back
-//             $hourBack.set('text', hourText);
-//
-//             (function () {
-//                 $hourTop.destroy();
-//                 $hourFront.destroy();
-//                 $hourBack.destroy();
-//
-//                 $hourTop = $newHourTop;
-//                 $hourFront = $newHourFront;
-//                 $hourBack = $newHourBack;
-//
-//                 $hourTop.setStyle('zIndex', 10);
-//                 $hourBottom.set('text', hourText);
-//             }).delay(800);
-//         }
-//
-//         if (currentMin != min) {
-//             currentMin = min;
-//             var minText = min < 10 ? '0' + min : min;
-//
-//             // make el to sit behind the top digit
-//             var $newMinTop = new Element('div', {class: 'digit-top', html: $minTop.get('html'), style: 'z-index:1;'})
-//             var $newMinFront = $newMinTop.getElement('div.front');
-//             var $newMinBack = $newMinTop.getElement('div.back');
-//
-//             $newMinFront.set('text', minText);
-//             $minWrap.adopt($newMinTop);
-//
-//             // start the animation
-//             $minFront.setTransform('rotateX(180deg)');
-//             $minBack.setTransform('rotateX(0deg)');
-//             $minBack.setStyle('zIndex', 40);
-//
-//             // set the min back
-//             $minBack.set('text', minText);
-//
-//             (function () {
-//                 $minTop.destroy();
-//                 $minFront.destroy();
-//                 $minBack.destroy();
-//
-//                 $minTop = $newMinTop;
-//                 $minFront = $newMinFront;
-//                 $minBack = $newMinBack;
-//
-//                 $minTop.setStyle('zIndex', 10);
-//                 $minBottom.set('text', minText);
-//             }).delay(800);
-//         }
-//
-//
-//         if (currentSec != sec) {
-//             currentSec = sec;
-//             var secText = sec < 10 ? '0' + sec : sec;
-//
-//             // make el to sit behind the top digit
-//             var $newSecTop = new Element('div', {class: 'digit-top', html: $secTop.get('html'), style: 'z-index:1;'})
-//             var $newSecFront = $newSecTop.getElement('div.front');
-//             var $newSecBack = $newSecTop.getElement('div.back');
-//
-//             $newSecFront.set('text', secText);
-//             $secWrap.adopt($newSecTop);
-//
-//             // start the animation
-//             $secFront.setTransform('rotateX(180deg)');
-//             $secBack.setTransform('rotateX(0deg)');
-//             $secBack.setStyle('zIndex', 40);
-//
-//             // set the min back
-//             $secBack.set('text', secText);
-//
-//             (function () {
-//                 $secTop.destroy();
-//                 $secFront.destroy();
-//                 $secBack.destroy();
-//
-//                 $secTop = $newSecTop;
-//                 $secFront = $newSecFront;
-//                 $secBack = $newSecBack;
-//
-//                 $secTop.setStyle('zIndex', 10);
-//                 $secBottom.set('text', secText);
-//             }).delay(800);
-//         }
-//
-//         //$hourEls.set('text', hour);
-//
-//     }
-//
-//     setClock.periodical(1000);
-// });
+                if (now !== value) {
+                    this._flip(slot, value);
+                }
+            }
+        },
 
+        _flip: function (slot, value) {
+
+            // setup new state
+            slot.classList.remove('flip');
+            slot.dataset.old = slot.dataset.now;
+            slot.dataset.now = value;
+
+            // force dom reflow
+            slot.offsetLeft;
+
+            // start flippin
+            slot.classList.add('flip');
+
+        }
+
+    };
+
+    return exports;
+}());
+
+var i = 0, clocks = document.querySelectorAll('.clock'), l = clocks.length;
+for (; i < l; i++) {
+    new Clock(clocks[i]);
+}
