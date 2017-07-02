8701bb83d1db5a8200c9d687ede4350fda4d49bb
L1n <490772448@qq.com>
2017-07-02 16:26:33 +0800

    编写测试, 继续改用代码编写 summary 而不是硬编码, 但是现在测试代码没写完, 模板逻辑倒是写好了, 视图函数还没写

--- my_blog/app_life_summary/tests/test_views.py
+++ my_blog/app_life_summary/tests/test_views.py
@@ -41,14 +41,68 @@ class TestLifeSummary(BasicTest):
             self.assertContains(response, '<div>{}</div>'.format(right_number))
             self.assertContains(response, '<div>{}</div>'.format(right_name))
 
-    def test_create_summary_right(self):
+    def test_create_summary_right_in_style_1(self):
         """
-        测试能够正确地创建页面的总结笔记
+        测试能够正确地创建页面的总结笔记(风格 1), 即类似于下面的情况
+    <h1>洗漱用品</h1>
+    <p>洗发水</p>
+    <ul>
+        <li>清扬，薄荷味，洗完确实没头屑，但是有油</li>
+        <li>沙宣，黑色，千万不要买；红色那款想试就试吧</li>
+        <li>xxx</li>
+    </ul>
         """
         response = self.client.get(self.unique_url)
 
         # 正常情况
         self.assertContains(response, '<h1>洗漱用品</h1>')
+
+        self.fail("还没写完")
+
+    def test_create_summary_right_in_style_2(self):
+        """
+        测试能够正确地创建页面的总结笔记(风格 2), 即类似于下面的情况
+    <h1>服装</h1>
+    <p>衣服</p>
+    <p>洗衣服</p>
+    <ul>
+        <li>洗衣袋
+            <ul>
+                <li>只用来洗袜子和内裤</li>
+                <li>买大一点的洗衣袋</li>
+            </ul>
+        </li>
+    </ul>
+        """
+        response = self.client.get(self.unique_url)
+
+        # 正常情况
+
+        self.fail("还没写完")
+
+    def test_create_summary_right_in_style_3(self):
+        """
+        测试能够正确地创建页面的总结笔记(风格 3), 即类似于下面的情况
+    <h1>服装</h1>
+    <p>衣服</p>
+    <ul>
+        <li>裤子 3 条不够用，起码 4 条</li>
+        <li style="list-style-type:none">
+            <ul>
+                <li>短裤可以去迪卡侬买拉链短裤</li>
+                <li>反光运动服迪卡侬有</li>
+                <li>裤子买跟亚马逊那条一样的材料，不会乱叫</li>
+            </ul>
+        </li>
+    </ul>
+        """
+        response = self.client.get(self.unique_url)
+
+        # 正常情况
+        self.assertContains(response, '<h1>洗漱用品</h1>')
+        self.assertContains(response, '<h1>洗漱用品</h1>')
+
+        self.fail("还没写完")
 
 
 if __name__ == "__main__":
