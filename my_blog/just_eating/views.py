cdf8862b061b1562e72923fcb7969dbebcf07d4c
L1n <490772448@qq.com>
2018-01-11 07:49:16 +0800

    更新菜单 + 修正无法访问的链接

--- my_blog/just_eating/views.py
+++ my_blog/just_eating/views.py
@@ -62,13 +62,13 @@ def create_school_menu():
 def create_sangfor_menu():
     sangfor_menu = list()
 
-    monday = const.EATING_MENU_STRUCTURE("周一", "公司\n粿条", "公司\n2 楼水煮鱼", "公司\n4 楼快餐")
-    tuesday = const.EATING_MENU_STRUCTURE("周二", "公司\n2 个面包 + 豆浆", "公司\n4 楼冒菜", "公司\n6 楼快餐")
-    wednesday = const.EATING_MENU_STRUCTURE("周三", "公司\n粥 + 鸡蛋", "公司\n6 楼粿条", "公司\n8 楼")
-    thursday = const.EATING_MENU_STRUCTURE("周四", "公司\n炒米粉 + 牛奶", "公司\n8 楼", "公司\n2 楼快餐")
-    friday = const.EATING_MENU_STRUCTURE("周五", "公司\n2 个面包 + 豆浆", "公司\n4 楼牛肉饭", "公司\n8 楼")
-    saturday = const.EATING_MENU_STRUCTURE("周六", "西丽\n双蛋肠粉", "公司\n6 楼 | -1 楼金百合", "不知道去哪吃")
-    sunday = const.EATING_MENU_STRUCTURE("周日", "西丽\n红糖馒头+奶黄包+豆浆", "公司\n订外卖", "不知道去哪吃")
+    monday = const.EATING_MENU_STRUCTURE("周一", "公司\n4 楼炒米粉", "公司\n2 楼酸汤牛肉鱼", "公司\n快餐")
+    tuesday = const.EATING_MENU_STRUCTURE("周二", "公司\n4 楼面包", "公司\n4 楼冒菜", "公司\n快餐")
+    wednesday = const.EATING_MENU_STRUCTURE("周三", "公司\n2 楼粿条", "公司\n金百味", "公司\n2 楼包子")
+    thursday = const.EATING_MENU_STRUCTURE("周四", "公司\n4 楼肠粉", "公司\n4 楼牛肉饭", "公司\n快餐")
+    friday = const.EATING_MENU_STRUCTURE("周五", "公司\n2 楼面包", "公司\n快餐", "公司\n8 楼")
+    saturday = const.EATING_MENU_STRUCTURE("周六", "西丽\n红糖馒头+奶黄包+豆浆", "公司\n金百合", "不知道去哪吃")
+    sunday = const.EATING_MENU_STRUCTURE("周日", "西丽\n双蛋肠粉", "公司\n订外卖", "不知道去哪吃")
 
     for each_day in [monday, tuesday, wednesday, thursday, friday, saturday, sunday]:
         sangfor_menu.append(each_day)
