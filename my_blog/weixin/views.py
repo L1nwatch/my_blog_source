b0c60e3d59c5d47981e25ec46af92f5e806e6a9e
L1n <490772448@qq.com>
2019-03-21 20:53:45 +0800

    公众号基本配置应该好了

--- my_blog/weixin/views.py
+++ my_blog/weixin/views.py
@@ -7,7 +7,9 @@
 # 标准库
 import logging
 import hashlib
-from django.shortcuts import render, redirect, Http404
+from django.shortcuts import render, redirect, Http404, HttpResponse
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
 
 # 自己的模块
 from common_module.common_help_function import log_wrapper
@@ -15,30 +17,15 @@ from common_module.common_help_function import log_wrapper
 logger = logging.getLogger("my_blog.weixin.views")
 
 
-def check_signature(signature, timestamp, nonce):
-    # L = [timestamp, nonce, 490772448]
-    # L.sort()
-    # s = L[0] + L[1] + L[2]
-    # hash_code = hashlib.sha1(s).hexdigest()
-    # return hash_code == signature
-    hash_list = ["wx1", timestamp, nonce]
-    hash_list.sort()
-    sha1 = hashlib.sha1()
-    map(sha1.update,hash_list)
-    hash_result = sha1.hexdigest()
-    if hash_result == signature:
-        return True
-    else:
-        return False
-
-
 @log_wrapper(str_format="服务器首次交互", level="info", logger=logger)
 def check_signature_from_server(this_request):
     if this_request.method == "GET":
-        signature = this_request.GET.get("signature")
-        echostr = this_request.GET.get("echostr")
-        timestamp = this_request.GET.get("timestamp")
-        nonce = this_request.GET.get("nonce")
-        # check_signature(signature, timestamp, nonce)
-        return echostr
-    raise Http404
+        signature = this_request.GET.get("signature", None)
+        echostr = this_request.GET.get("echostr", None)
+        timestamp = this_request.GET.get("timestamp", None)
+        nonce = this_request.GET.get("nonce", None)
+        try:
+            check_signature("lf490772448", signature, timestamp, nonce)
+            return HttpResponse(echostr)
+        except InvalidSignatureException as e:
+            raise Http404
