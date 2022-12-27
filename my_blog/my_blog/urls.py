afacce678fe33e02aabebc47a494ba7245c8bfc0
L1nwatch <490772448@qq.com>
2022-12-27 16:07:02 +0800

    [2022-12-27] update cv urls

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -48,6 +48,7 @@ urlpatterns = [
     url(r'^$', homepage.views.index_view, name='index'),
     url(r'^googlef0b96351a9e6fd45\.html$', articles.views.google_verify, name='google_verify'),
     url(r'^resume$', articles.views.resume, name='resume'),
+    url(r'^cv$', articles.views.cv, name='cv'),
     url(r"^search/$", articles.views.blog_search, name="search"),
     url(r"^tool_hub/", include("toolhub.urls")),
     url(r"^code_collect/", include("code_collect.urls")),
