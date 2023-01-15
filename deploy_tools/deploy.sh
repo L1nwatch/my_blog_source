e1a01275f23e4db93a7a8c16956f866be99788e1
L1nwatch <490772448@qq.com>
2023-01-15 16:37:04 +0800

    [2023-01-15] write an deploy shell script

---
+++ deploy_tools/deploy.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+sudo apt-get update
+sudo apt-get install nginx
+sudo apt-get install supervisor
+sudo apt-get install git
+sudo apt-get install python3.9
+sudo apt-get install python3.9-pip
+sudo apt-get install fabric
+sudo apt-get install python3-dev
+sudo pip3 install virtualenv
