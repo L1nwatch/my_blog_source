61e62650684df7e5b5ffaa1d5a352c61b1bdb423
L1nwatch <490772448@qq.com>
2023-01-21 17:18:34 +0800

    [2023-01-21] update ubuntu install python3.9 steps

--- deploy_tools/deploy.sh
+++ deploy_tools/deploy.sh
@@ -3,7 +3,9 @@
 sudo apt-get update
 sudo apt-get install nginx
 sudo apt-get install supervisor
-sudo apt-get install python3.9
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
 sudo apt-get install python3.9-dev
 sudo apt-get install python3.9-venv
 sudo apt install python3.9-distutils
